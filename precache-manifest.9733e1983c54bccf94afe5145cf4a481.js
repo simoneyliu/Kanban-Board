b7056b162d089cb8cf93384c83bffa067f8401b5
Simone Liu <simoneliu94@gmail.com>
2021-05-24 09:18:17 -0400

    Updates

---
+++ precache-manifest.9733e1983c54bccf94afe5145cf4a481.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e10d3e8656ceaa04d574fdbb36ee89",
+    "url": "/Kanban-Board/index.html"
+  },
+  {
+    "revision": "0b545af39474184728b3",
+    "url": "/Kanban-Board/static/css/4.17f8cf15.chunk.css"
+  },
+  {
+    "revision": "e1af7148666b71d02f69",
+    "url": "/Kanban-Board/static/css/main.256b88cc.chunk.css"
+  },
+  {
+    "revision": "0b545af39474184728b3",
+    "url": "/Kanban-Board/static/js/4.af5e54c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kanban-Board/static/js/4.af5e54c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e362b6069429405f9ef",
+    "url": "/Kanban-Board/static/js/5.393f7f73.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/Kanban-Board/static/js/5.393f7f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3447aefb73c3113cb713",
+    "url": "/Kanban-Board/static/js/6.832328b1.chunk.js"
+  },
+  {
+    "revision": "e1af7148666b71d02f69",
+    "url": "/Kanban-Board/static/js/main.7c1da14a.chunk.js"
+  },
+  {
+    "revision": "661dcfe019ac6a911d02",
+    "url": "/Kanban-Board/static/js/runtime-main.2b222cc1.js"
+  },
+  {
+    "revision": "e99d0b3f28656984d844",
+    "url": "/Kanban-Board/static/js/stencil-h8k-logo_2-entry-js.6a47270f.chunk.js"
+  },
+  {
+    "revision": "aff4113d40ce0955293b",
+    "url": "/Kanban-Board/static/js/stencil-polyfills-css-shim.c447c352.chunk.js"
+  }
+]);
